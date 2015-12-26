db18678ea6690ecf4ff196d35db541010d210891
weiyu.lin <weiyu.in@rakuten.com>
2015-12-26 14:23:13 +0800

    add custom preParser feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var loaderUtils = require('loader-utils');
 var fetchUrl = require("fetch").fetchUrl;
+var __ = require("lodash");
 
 module.exports = function(content) {
     this.cacheable && this.cacheable();
@@ -8,26 +9,34 @@ module.exports = function(content) {
     var callback = this.async();
     var query = loaderUtils.parseQuery(this.query);
 
+    //////////////////////
+    //Set up the pre parser
+    ///////////////////////
 
-    /*Set up the pre parser before return*/
-    var preParser;
-    switch (query.preParser) {
-        case "rt":
-            preParser = require("./preParser/reactTemplate.js");
-            break;
-        default:
-            preParser = function(rawContent) {
-                return rawContent;
-            };
+    //Check the custom preparser first
+    var preParser = __.get(this.options, 'NodeAsycRequirePreParser[' + query.preParser + ']');
+
+    //If there if no custom preParser then check the offical preParsers
+    if (typeof preParser === "undefined") {
+        switch (query.preParser) {
+            case "rt":
+                preParser = require("./preParser/reactTemplate.js");
+                break;
+            default:
+                preParser = function(rawContent) {
+                    return rawContent;
+                };
+        }
     }
-
-    console.log(preParser);
-
+    //////////////////////////////////////////////
+    //Set up the node module url with queryString
+    /////////////////////////////////////////////
     var queryString = query.queryString || "";
-    var nodeModleUrl = content.trim() + queryString.replace(/'/gi, "");
+    var nodeModleUrl = content.trim() + queryString.replace(/'/gi, ""); //trim the contents and remove the ''
 
-    /*Do the aync fetch and return*/
-    console.log(nodeModleUrl);
+    //Do the aync fetch and return
+    console.log("Remote Node Module Url:" + nodeModleUrl);
+
     fetchUrl(nodeModleUrl, function(err, meta, body) {
         var rawContent = body.toString();
         callback(null, "module.exports = " + JSON.stringify(preParser(rawContent)));
