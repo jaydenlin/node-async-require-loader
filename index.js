9c264455496b3ed122f60c858aeb3dc122347415
weiyu.lin <weiyu.in@rakuten.com>
2016-01-01 23:53:52 +0800

    fix webpack-dev-server watch break issue

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-var loaderUtils = require('loader-utils');
-var fetchUrl = require("fetch").fetchUrl;
-var __ = require("lodash");
-var fs = require("fs");
-
-module.exports = function(content) {
-    this.cacheable && this.cacheable();
-    this.value = content;
-
-    var callback = this.async();
-    var query = loaderUtils.parseQuery(this.query);
-    var async = (typeof(query.async)!=='undefined')? query.async:true;
-    var content;
-    try {
-      content = eval(content);
-    } catch (e) {
-      throw new Error('The .ajs file is not a valid js. ' + e.toString());
-    }
-
-    if (Object.prototype.toString.call(content) !== '[object Object]') {
-      throw new Error('The .ajs file does not return a valid json');
-    } else if (typeof content.remoteUrl === "undefined") {
-      throw new Error('The .ajs file must return a remoteUrl');
-    }
-    //////////////////////
-    //Set up the pre parser
-    ///////////////////////
-
-    //Check the custom preparser first
-    var preParser = __.get(this.options, 'NodeAsycRequirePreParser[' + query.preParser + ']');
-
-    // if(typeof(query.async)!=='undefined' && !query.async){
-
-    // }
-    
-
-    //If there if no custom preParser then check the offical preParsers
-    if (typeof preParser === "undefined") {
-        switch (query.preParser) {
-            case "rt":
-                preParser = require("./preParser/reactTemplate.js");
-                break;
-            default:
-                preParser = function(rawContent) {
-                    return rawContent;
-                };
-        }
-    }
-    //////////////////////////////////////////////
-    //Set up the node module url with queryString
-    /////////////////////////////////////////////
-    var queryString = query.queryString || "";
-    var nodeModleUrl = content.remoteUrl.trim() + queryString.replace(/'/gi, ""); //trim the contents and remove the ''
-    //Do the aync fetch and return
-    console.log("Remote Node Module Url:" + nodeModleUrl);
+ "use strict";
+ var loaderUtils = require('loader-utils');
+ var __ = require("lodash");
+ var path = require("path");
+ var _eval = require("eval");
+ var requestSync = require("sync-request");
+ var fs = require("fs");
 
 
-    if(async){
-        fetchUrl(nodeModleUrl, function(err, meta, body) {
-            var rawContent = body.toString();
-            // callback(null, "module.exports = " + JSON.stringify(preParser(rawContent)));
+ module.exports = function(content) {
 
-            callback(null, preParser(rawContent));
+     var that = this;
+     var query = loaderUtils.parseQuery(this.query);
+     var async = (typeof(query.async) !== 'undefined') ? query.async: true;
 
-        });    
-    }else{
-        fs.readFile(content.localPath, function (err, data){
-            callback(null, preParser(data));
-        });
-        
-    }
-    
+     try {
+         content = _eval(content);
+     } catch (e) {
+         throw new Error('The .ajs file is not a valid js. ' + e.toString());
+     }
 
-}
+     if (Object.prototype.toString.call(content) !== '[object Object]') {
+         throw new Error('The .ajs file does not return a valid json');
+     } else if (typeof content.remoteUrl === "undefined") {
+         throw new Error('The .ajs file must return a remoteUrl');
+     }
+     //////////////////////
+     //Set up the pre parser
+     ///////////////////////
+
+     //Check the custom preparser first
+     var preParser = __.get(this.options, 'NodeAsycRequirePreParser[' + query.preParser + ']');
+
+     //If there if no custom preParser then check the offical preParsers
+     if (typeof preParser === "undefined") {
+         switch (query.preParser) {
+             case "rt":
+                 preParser = require("./preParser/reactTemplate.js");
+                 break;
+             default:
+                 preParser = function(rawContent) {
+                     return rawContent;
+                 };
+         }
+     }
+     //////////////////////////////////////////////
+     //Set up the node module url with queryString
+     /////////////////////////////////////////////
+     var queryString = query.queryString || "";
+     var nodeModuleUrl = content.remoteUrl.trim() + queryString.replace(/'/gi, ""); //trim the contents and remove the ''
+
+     if (async) {
+         //Do the aync fetch and return
+         console.log("Remote Node Module Url:" + nodeModuleUrl);
+         var res = requestSync('GET', nodeModuleUrl);
+         var rawContent = res.getBody('utf8');
+         var source = preParser(rawContent);
+
+         that.callback(null, source);
+
+     } else {
+         //load local file
+         var rawContent = fs.readFileSync(content.localPath, {
+             encoding: 'utf8'
+         });
+
+         var source = preParser(rawContent);
+         that.resourcePath = content.localPath;
+         that.resource = content.localPath;
+         that.callback(null, source);
+
+     }
+
+
+ };
